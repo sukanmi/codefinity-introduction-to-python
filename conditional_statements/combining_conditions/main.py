190f80444a82f844c3ee6e781fa21aafb9000cad
sukanmi <>
2025-11-19 17:06:59 +0000

    Combining Conditions

--- conditional_statements/combining_conditions/main.py
+++ conditional_statements/combining_conditions/main.py
@@ -1,16 +1,10 @@
-# The item's discount and stock status have been defined
-#discounted = False
-#lowStock = True
-# Define the perishable and stock status conditions
-is_perishable = True
-item_quantity = 110
-perishable_highStockRisk = 100
+# Define the seasonal and holiday status conditions 
+seasonal_item = False
+holiday_item = True
 
-# Using the (and) operator to combine two conditions
-# The first condition (`is_perishable`) checks if the item is perishable
-# The second condition (`item_quantity >= perishable_highStockRisk`) checks if the item is high in stock 
-# The `consider_discount` variable will become `True` only if both conditions are `True`
-consider_discount = is_perishable and (item_quantity >= perishable_highStockRisk)
+# Combine the conditions to check if the item is seasonal or discounted
+# (`temporary_stock`) will become `True` if either condition `seasonal_item` OR `holiday_item` is `True`
+temporary_stock = seasonal_item or holiday_item
 
 # Print the result
-print("Is the item perishable and high in stock?", consider_discount)
+print("Is this a seasonal or holiday item?", temporary_stock) 
