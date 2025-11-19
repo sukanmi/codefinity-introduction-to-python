f727055cf40e2d7fe2f2615d2e48fd3821950f54
sukanmi <>
2025-11-19 17:07:36 +0000

    Combining Conditions

--- conditional_statements/combining_conditions/main.py
+++ conditional_statements/combining_conditions/main.py
@@ -1,10 +1,8 @@
-# Define the seasonal and holiday status conditions 
-seasonal_item = False
-holiday_item = True
+# Define the item status condition
+is_perishable = True
 
-# Combine the conditions to check if the item is seasonal or discounted
-# (`temporary_stock`) will become `True` if either condition `seasonal_item` OR `holiday_item` is `True`
-temporary_stock = seasonal_item or holiday_item
+# Use the `not` operator to check if the item is NOT perishable
+long_shelf_life = not is_perishable
 
 # Print the result
-print("Is this a seasonal or holiday item?", temporary_stock) 
+print("Does the item need to be sold quickly?", long_shelf_life) 
